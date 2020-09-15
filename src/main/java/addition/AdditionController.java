6ae6b988186a43f295ced521ed223e124b64da6e
Carsten Michel <carsten.michel@de.ibm.com>
2020-09-15 14:27:24 +0200

    small changes

--- src/main/java/addition/AdditionController.java
+++ src/main/java/addition/AdditionController.java
@@ -1,21 +1,21 @@
 package addition;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class AdditionController {
     
-    @RequestMapping("/addition")
-    public String add(@RequestParam(value="number1",defaultValue="1")int n1,
+    @RequestMapping(path= "/addition", consumes = MediaType.APPLICATION_JSON_VALUE, 
+    produces = MediaType.APPLICATION_JSON_VALUE, 
+    method = RequestMethod.GET)
+    public int add(@RequestParam(value="number1",defaultValue="1")int n1,
     @RequestParam(value="number2", defaultValue="2") int n2) {
         AdditionShop as = new AdditionShop(n1, n2);
-        StringBuilder sb=new StringBuilder("Addition of ");
-        sb.append(as.getNumber1()).append(" and ");
-        sb.append(as.getNumber2()).append(" is ");
-        sb.append(as.getAddition());
-        return sb.toString();
+        return as.getAddition();
     }
     
 }
