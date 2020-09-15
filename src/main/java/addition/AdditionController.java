98e8f30f13308cbed2019a1978dfa4c5b9fce291
Carsten Michel <carsten.michel@de.ibm.com>
2020-09-15 15:17:39 +0200

    add logging

--- src/main/java/addition/AdditionController.java
+++ src/main/java/addition/AdditionController.java
@@ -1,21 +1,26 @@
 package addition;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.helpers.BasicMarkerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdditionController {
-    
-    @RequestMapping(path= "/addition", consumes = MediaType.APPLICATION_JSON_VALUE, 
-    produces = MediaType.APPLICATION_JSON_VALUE, 
-    method = RequestMethod.GET)
-    public int add(@RequestParam(value="number1",defaultValue="1")int n1,
-    @RequestParam(value="number2", defaultValue="2") int n2) {
-        AdditionShop as = new AdditionShop(n1, n2);
-        return as.getAddition();
+
+    Logger logger = LoggerFactory.getLogger(AdditionController.class);
+
+    @GetMapping(path = "/addition", produces = { "application/json" })
+    public String add(@RequestParam(value = "number1", defaultValue = "1") final int n1,
+            @RequestParam(value = "number2", defaultValue = "2") final int n2) {
+
+        final Marker addingMarker = new BasicMarkerFactory().getMarker("ADDER");
+        final AdditionShop as = new AdditionShop(n1, n2);
+        logger.trace(addingMarker, "Adding numbers n1={} and n2={}", n1, n2);
+        return Integer.toString(as.getAddition());
     }
-    
+
 }
