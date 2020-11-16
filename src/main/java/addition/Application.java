e2edd5f289efdb1a001b606d4365fa6fcf215a99
Carsten Michel <carstenmichel@users.noreply.github.com>
2020-11-16 15:13:48 +0100

    upgrade boot

--- src/main/java/addition/Application.java
+++ src/main/java/addition/Application.java
@@ -2,14 +2,13 @@ package addition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
-    
-    public static void main(String[] args) {
-        // not needed : ApplicationContext ctx = 
-        SpringApplication.run(Application.class, args);
-    }
+
+ 
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
